e867523b93feccb35d637c3bfdb01721456e2dd5
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-12-19 22:25:32 -0300

    Agregamos el componente TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import TituloSecundario from "./components/TituloSecundario";
 
 function App() {
+  //Aqui agregamos toda la logica que necesite el componente
+
   return (
-   <h1 className="display-3">Titulo de prueba</h1>
-  )
+    //Aqui puedo agregar un poco de logica
+    <>
+      <section className="container my-4">
+        <h1 className="display-3 text-center">Contador con React</h1>
+        <TituloSecundario></TituloSecundario>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TituloSecundario.jsx
@@ -0,0 +1,18 @@
+const TituloSecundario = () => {
+  return (
+    <article>
+      <h2>Practica con react</h2>
+      <p>
+        React.js, comúnmente llamado simplemente React, es una biblioteca de
+        JavaScript que se utiliza para construir interfaces de usuario. Toda
+        aplicación web React se compone de componentes reutilizables que
+        conforman partes de la interfaz de usuario — podemos tener un componente
+        distinto para nuestra barra de navegación, otro para el pie de página,
+        otro para el contenido principal, etc. Entenderás mejor esto cuando
+        lleguemos a la sección en la que tenemos que trabajar con componentes.
+      </p>
+    </article>
+  );
+};
+
+export default TituloSecundario;
