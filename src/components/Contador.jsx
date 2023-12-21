fd820979c15e3e1b432a6811861ffe479e3266be
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2023-12-21 16:03:57 -0300

    Realice el estado contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Contador = () => {
-    let numero = 0;
+    const [numero, setNumero] = useState(0);
+    // let numero = 0;
     return (
         <article className="text-center">
             <h2>Contador</h2>
             <h3>{numero}</h3>
-            <button className="btn btn-primary" onClick={() => {numero + 1}}>+1</button>
+            <button className="btn btn-primary" onClick={() => {setNumero(numero + 1)}}>+1</button>
         </article>
     );
 };
